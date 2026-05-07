5bfd6d51f1c7aaae13cb384e455bc85eafd53a32
Antonio Henrique Macan <toninhm96@gmail.com>
2026-05-07 00:00:27 -0300

    improve theme settings dialog screen

--- src/app/settings/theme.tsx
+++ src/app/settings/theme.tsx
@@ -1,8 +1,9 @@
 import { setThemeMode, ThemeMode, useSettingsStore } from "@/stores/settings";
+import { sizes } from "@/styles/sizes";
 import { router } from "expo-router";
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
-import { Button, Dialog, RadioButton } from "react-native-paper";
+import { Button, Dialog, List, RadioButton } from "react-native-paper";
 
 const ThemeModeScreen = () => {
   const { t } = useTranslation();
@@ -10,6 +11,10 @@ const ThemeModeScreen = () => {
   const currentThemeMode = useSettingsStore((s) => s.themeMode);
 
   const [newThemeMode, setNewThemeMode] = useState<ThemeMode>(currentThemeMode);
+
+  const onValueChange = (value: string) => {
+    setNewThemeMode(value as ThemeMode);
+  };
 
   const onDismiss = () => {
     router.back();
@@ -24,10 +29,28 @@ const ThemeModeScreen = () => {
     <Dialog visible onDismiss={onDismiss}>
       <Dialog.Title>{t("Tema")}</Dialog.Title>
       <Dialog.Content>
-        <RadioButton.Group value={newThemeMode} onValueChange={setNewThemeMode}>
-          <RadioButton.Item value="auto" label={t("Auto")} />
-          <RadioButton.Item value="light" label={t("Claro")} />
-          <RadioButton.Item value="dark" label={t("Escuro")} />
+        <RadioButton.Group value={newThemeMode} onValueChange={onValueChange}>
+          <List.Item
+            left={() => <RadioButton value="auto" />}
+            style={{ paddingBlock: sizes.xxs }}
+            contentStyle={{ paddingLeft: sizes.xs }}
+            title={t("Auto")}
+            onPress={() => setNewThemeMode("auto")}
+          />
+          <List.Item
+            left={() => <RadioButton value="light" />}
+            style={{ paddingBlock: sizes.xxs }}
+            contentStyle={{ paddingLeft: sizes.xs }}
+            title={t("Claro")}
+            onPress={() => setNewThemeMode("light")}
+          />
+          <List.Item
+            left={() => <RadioButton value="dark" />}
+            style={{ paddingBlock: sizes.xxs }}
+            contentStyle={{ paddingLeft: sizes.xs }}
+            title={t("Escuro")}
+            onPress={() => setNewThemeMode("dark")}
+          />
         </RadioButton.Group>
       </Dialog.Content>
       <Dialog.Actions>
