3402c538e61aeb491d663d163e23d8b4286c8dd1
Antonio Henrique Macan <toninhm96@gmail.com>
2026-05-14 20:45:02 -0300

    upgrade expo sdk to 56

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -1,31 +1,15 @@
-import {
-  MD3DarkTheme,
-  MD3LightTheme,
-  adaptNavigationTheme,
-} from "react-native-paper";
-
-import {
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationLightTheme,
-} from "@react-navigation/native";
+import { MD3DarkTheme, MD3LightTheme } from "react-native-paper";
 
 import { fontConfig } from "./fonts";
 
-const { DarkTheme, LightTheme } = adaptNavigationTheme({
-  reactNavigationDark: NavigationDarkTheme,
-  reactNavigationLight: NavigationLightTheme,
-});
-
 export const darkTheme = {
-  ...DarkTheme,
   ...MD3DarkTheme,
-  colors: { ...DarkTheme.colors, ...MD3DarkTheme.colors },
+  colors: { ...MD3DarkTheme.colors },
   fonts: fontConfig,
 };
 
 export const lightTheme = {
-  ...LightTheme,
   ...MD3LightTheme,
-  colors: { ...LightTheme.colors, ...MD3LightTheme.colors },
+  colors: { ...MD3LightTheme.colors },
   fonts: fontConfig,
 };
